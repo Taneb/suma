b9386ffa6c978f64e3313493acf90f1ed73165cd
Nathan van Doorn <nvd1234@gmail.com>
2020-01-15 21:51:33 +0000

    Add a test suite
    We test that any formula generated to be satisfiable is marked as
    satisfiable by our algorithm.

---
+++ test/Test.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import Control.Monad
+import Test.QuickCheck
+import Test.Hspec
+
+import Suma
+
+-- generate known-to-be-satisfiable formulas by coming up with an assignment of
+-- variables and putting one of the variables from that assignment in each
+-- clause.
+genSatisfiableFormula :: Gen Formula
+genSatisfiableFormula = do
+  nVars <- arbitrarySizedNatural `suchThat` (> 0)
+  assignments <- zip [0..] <$> replicateM nVars arbitraryBoundedEnum
+  nClauses <- arbitrarySizedNatural `suchThat` (> 0)
+  replicateM nClauses $ do
+    nPre <- scale (\n -> round (sqrt (fromIntegral n) / 2))
+      arbitrarySizedNatural
+    pre  <- replicateM nPre $
+      (,) <$> choose (0, nVars - 1) <*> arbitraryBoundedEnum
+    var <- elements assignments
+    nPost <- scale (\n -> round (sqrt (fromIntegral n) / 2))
+      arbitrarySizedNatural
+    post <- replicateM nPost $
+      (,) <$> choose (0, nVars - 1) <*> arbitraryBoundedEnum
+    pure $ pre ++ [var] ++ post
+
+satSatisfiable :: Property
+satSatisfiable = forAll genSatisfiableFormula sat
+
+main :: IO ()
+main = hspec $ do
+  describe "sat" $ do
+    it "Satisfiable formulas are satisfiable" $
+      property satSatisfiable
