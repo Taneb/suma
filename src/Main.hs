00668eedcedc2ff84ff6ee8323abbe7aefc85251
Aiken Cairncross <acairncross@gmail.com>
2020-01-25 13:44:57 +0000

    Print errors to stderr instead of stdout

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
-import System.Exit (exitFailure, exitSuccess)
+import System.Exit (die, exitSuccess)
 
 import Dimacs (parseDimacsCnf)
 import Suma (sat)
@@ -9,13 +9,11 @@ main = do
   args <- getArgs
   case args of
     [] -> do
-      putStrLn "Usage: suma [FILE]"
-      exitFailure
+      die "Usage: suma [FILE]"
     dimacsFile:_ -> do
       dimacsText <- TIO.readFile dimacsFile
       case parseDimacsCnf dimacsText of
         Nothing -> do
-          putStrLn "Failed to parse DIMACS file"
-          exitFailure
+          die "Failed to parse DIMACS file"
         Just formula ->
           putStrLn $ if sat formula then "SAT" else "UNSAT"
