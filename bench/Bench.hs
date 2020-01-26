1c24302e2545c00116718f1e99c2545f4179d25c
Aiken Cairncross <acairncross@gmail.com>
2020-01-26 09:56:00 +0000

    Add benchmarks

---
+++ bench/Bench.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Criterion.Main
+import qualified Data.Text.IO as TIO
+import System.Exit (die)
+import System.FilePath
+
+import Dimacs (parseDimacsCnf)
+import Suma.Types (Formula)
+import Suma (solve)
+
+import Paths_suma (getDataFileName)
+
+blocksworldDir :: FilePath
+blocksworldDir = "examples" </> "blocksworld"
+
+blocksworldBenchmark :: String -> Benchmark
+blocksworldBenchmark name =
+  env setup $ \formula -> bench name $ whnf solve formula
+  where
+    setup :: IO Formula
+    setup = do
+      testFile <- getDataFileName $ blocksworldDir </> name <.> "cnf"
+      testCase <- TIO.readFile testFile
+      case parseDimacsCnf testCase of
+        Nothing -> die "Failed to parse DIMACS file"
+        Just formula -> pure formula
+
+main = defaultMain
+  [ blocksworldBenchmark "anomaly"
+  , blocksworldBenchmark "medium"
+  ]
